46d12e333fbcfe332197ff38980d28fcb37b6685
revo667 <revo667@proton.me>
2026-09-02 14:12:02 +0200

    version change

--- core/version.py
+++ core/version.py
@@ -1,7 +1,7 @@
 
 from __future__ import annotations
 
-APP_VERSION = "0.3.0"
+APP_VERSION = "0.3.1"
 
 #: CI paketleme oncesi bu satiri yeniden yazar (build.yml -> "Surum damgasi").
 BUILD_SHA = ""
